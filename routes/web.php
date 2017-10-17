9c6217d9884b0bcb7dfcdf78a8694d7a41d9377f
廖文忠 <wcliao1962@gmail.com>
2017-10-17 21:36:04 +0800

    改用HomeController@index 回傳視圖(home.index)

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home.index');
-});
+Route::get('/home', 'HomeController@index');
