4bfe764b73d4d89ff246bd1eac62af7f1fea70e8
廖文忠 <wcliao1962@gmail.com>
2017-10-25 00:06:39 +0800

    練習七: 運用migration調整資料表結構

---
+++ database/migrations/2017_10_24_155018_add_is_feature_in_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsFeatureInPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('title', 500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
