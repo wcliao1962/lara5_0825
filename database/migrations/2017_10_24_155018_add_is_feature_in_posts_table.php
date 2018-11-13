3a36658114fa0a2522de76d56e9e3da9d35b2a11
廖文忠 <wcliao1962@gmail.com>
2018-11-13 22:39:01 +0800

    在down()加入與up()相反的動作

--- database/migrations/2017_10_24_155018_add_is_feature_in_posts_table.php
+++ database/migrations/2017_10_24_155018_add_is_feature_in_posts_table.php
@@ -29,6 +29,8 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
+            $table ->string('title') ->change();
+            $table ->dropColumn('is_feature') ;
         });
     }
 }
