7d7297d832d9b70aabf469145cff4d9c2c1a649c
廖文忠 <wcliao1962@gmail.com>
2018-11-13 23:56:52 +0800

    練習5-1

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,14 @@ Route::get('/', function () {
 //        'content' => 'updated content',
 //    ]);
 
+//    $post = \App\Post::find(1);
+//    $post->title = 'saved title';
+//    $post->content = 'savted content';
+//    $post->save();
+
     $post = \App\Post::find(1);
-    $post->title = 'saved title';
-    $post->content = 'savted content';
-    $post->save();
+    $post->delete();
+
 
 
 
