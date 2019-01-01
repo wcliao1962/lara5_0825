ff7d4e17c87b16cd0117fe0aeb26ffdc8f3846d7
廖文忠 <wcliao1962@gmail.com>
2019-01-01 22:05:00 +0800

    練習2, 練習3

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach(range(1, 20) as $number) {
+            Post::create([
+                'title' => 'title '.$number,
+                'content' => 'content '.$number,
+            ]);
+        }
     }
 }
